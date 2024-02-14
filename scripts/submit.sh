6fce60f5a5ff6184fb0fd8d448821838cb12e246
Breno Hildebrand <hello@brenohildebrand.com>
2024-02-14 17:34:10 -0300

    ok

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -4,6 +4,7 @@ PROJECTPWD=$OLDPWD
 SCRIPTPWD=$PWD
 FRAMEWORKPWD=$(dirname $SCRIPTPWD)
 
+NAME=$(basename $PROJECTPWD | sed "s/^ft_//")
 if [ "$PROJECTPWD" = "$FRAMEWORKPWD" ]; then
 	echo "You're in the ft_framework directory. Nothing done."
 	exit 0
@@ -26,9 +27,67 @@ else
 	mkdir -p submit/lib/ft_framework
 	
 	cp -r ${FRAMEWORKPWD}/source submit/lib/ft_framework/
-	echo '
-	# TODO: Makefile
-	' > submit/lib/ft_framework/Makefile
+	cp -r ${FRAMEWORKPWD}/includes submit/lib/ft_framework/
+	echo "# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: $(date +'%Y/%m/%d %H:%M:%S') by bhildebr          #+#    #+#              #
+#    Updated: $(date +'%Y/%m/%d %H:%M:%S') by bhildebr         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = build/libframework.a
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+
+SOURCES = \\
+$(find $FRAMEWORKPWD/source/base/processes -type f -exec echo -e '\t'{} \\ \; | sed 's/.*ft_framework\//\t/' | sed '/.*\.h.*/d')
+$(find $FRAMEWORKPWD/source/base/types -type f -exec echo -e '\t'{} \\ \; | sed 's/.*ft_framework\//\t/' | sed '/.*\.h.*/d')
+$(find $FRAMEWORKPWD/source/processes -type f -exec echo -e '\t'{} \\ \; | sed 's/.*ft_framework\//\t/' | sed '/.*\.h.*/d')
+$(find $FRAMEWORKPWD/source/types -type f -exec echo -e '\t'{} \\ \; | sed 's/.*ft_framework\//\t/' | sed '/.*\.h.*/d' | sed '$s/ \\//')
+
+OBJECTS = \\
+$(find $FRAMEWORKPWD/source/base/processes -type f -exec basename {} \; | sed 's/\.c$/.o/' | sed 's/.*/\tobjects\/& \\/' | sed '/.*\.h.*/d')
+$(find $FRAMEWORKPWD/source/base/types -type f -exec basename {} \; | sed 's/\.c$/.o/' | sed 's/.*/\tobjects\/& \\/' | sed '/.*\.h.*/d')
+$(find $FRAMEWORKPWD/source/processes -type f -exec basename {} \; | sed 's/\.c$/.o/' | sed 's/.*/\tobjects\/& \\/' | sed '/.*\.h.*/d')
+$(find $FRAMEWORKPWD/source/types -type f -exec basename {} \; | sed 's/\.c$/.o/' | sed 's/.*/\tobjects\/& \\/' | sed '/.*\.h.*/d' | sed '$s/ \\//')
+
+INCLUDES = \\
+	-include ft_framework.h \\
+	-iquote includes \\
+$(find $FRAMEWORKPWD/source/base/processes -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_framework\// /')
+$(find $FRAMEWORKPWD/source/base/types -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_framework\// /')
+$(find $FRAMEWORKPWD/source/processes -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_framework\// /')
+$(find $FRAMEWORKPWD/source/types -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_framework\// /' | sed '$s/ \\//')
+
+all: \$(NAME)
+
+\$(NAME): \$(OBJECTS)
+	ar rcs \$(NAME) \$(OBJECTS)
+
+directories:
+	mkdir -p build
+	mkdir -p objects
+
+clean:
+	\$(RM) \$(OBJECTS)
+
+fclean: clean
+	\$(RM) \$(NAME)
+
+re: fclean all
+
+%.o: %.c
+	\$(CC) \$(CFLAGS) \$(INCLUDES) -c $< -o $@
+
+.PHONY: \$(NAME) all directories clean fclean re
+
+" > submit/lib/ft_framework/Makefile
 
 	cp -r ./source ./submit/
 	echo "# **************************************************************************** #
@@ -43,37 +102,41 @@ else
 #                                                                              #
 # **************************************************************************** #
 
-NAME = build/${basename $PROJECTPWD | sed "s/^ft_//"}
-FRAMEWORK = libs/ft_framework/build/libframework.a
+NAME = build/${NAME}
+FRAMEWORK = lib/ft_framework/build/libframework.a
 
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror
 
-SOURCES = \
-	$(find $PROJECTPWD/source/processes -type f -name "*.c" -exec echo {} \\ \; | sed 's/ \\$//')
-	$(find $PROJECTPWD/source/types -type f -name "*.c" -exec echo {} \\ \; | sed 's/ \\$//')
+SOURCES = \\
+$(find $PROJECTPWD/source/processes -type f -name "*.c" -exec echo -e '\t'{} \\ \; | $(echo sed 's/.*ft_'${NAME}'\//\t/'))
+$(find $PROJECTPWD/source/types -type f -name "*.c" -exec echo -e '\t'{} \\ \; | sed '$s/ \\$//' | $(echo sed 's/.*ft_'${NAME}'\//\t/'))
 
-OBJECTS = \
-	$(find $PROJECTPWD/source/processes -type f -name "*.c" -exec echo {} \\ \; | sed 's/ \\$//' | sed 's/.c \\$/.o \\/' | sed 's/.c$/.o/')
-	$(find $PROJECTPWD/source/types -type f -name "*.c" -exec echo {} \\ \; | sed 's/ \\$//' | sed 's/.c \\$/.o \\/' | sed 's/.c$/.o/')
+OBJECTS = \\
+$(find $PROJECTPWD/source/processes -type f -name "*.c" -exec basename {} \; | sed 's/^/\tobjects\//' | sed 's/\.c$/.o \\/')
+$(find $PROJECTPWD/source/types -type f -name "*.c" -exec basename {} \; | sed 's/^/\tobjects\//' | sed 's/\.c$/.o \\/' | sed '$s/ \\//')
 
-INCLUDES = \
-	-include ft_framework.h \
-
-	-iquote lib/ft_framework/base/processes/** \
-	-iquote lib/ft_framework/base/types/** \
-	-iquote lib/ft_framework/processes/** \
-	-iquote lib/ft_framework/types/** \
-	-iquote source/processes/** \
-	-iquote source/types/** \
+INCLUDES = \\
+	-include ft_framework.h \\
+	-iquote lib/ft_framework/includes \\
+$(find $FRAMEWORKPWD/source/base/processes -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_framework\// lib\/ft_framework\//')
+$(find $FRAMEWORKPWD/source/base/types -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_framework\// lib\/ft_framework\//')
+$(find $FRAMEWORKPWD/source/processes -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_framework\// lib\/ft_framework\//')
+$(find $FRAMEWORKPWD/source/types -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_framework\// lib\/ft_framework\//')
+$(find $PROJECTPWD/source/processes -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_'${NAME}'\// /')
+$(find $PROJECTPWD/source/types -mindepth 1 -type d -exec echo -e '\t'-iquote {} \\ \; | sed 's/ .*ft_'${NAME}'\// /' | sed '$s/ \\$//')
 
 all: \$(NAME)
 
 \$(NAME): \$(FRAMEWORK) \$(OBJECTS)
 	@cc -o \$(NAME) \$(OBJECTS) \$(FRAMEWORK)
-	
+
 \$(FRAMEWORK):
-	\$(MAKE) -C libs/ft_framework
+	\$(MAKE) -C lib/ft_framework
+
+directories:
+	mkdir -p build
+	mkdir -p objects
 
 clean:
 	\$(RM) \$(OBJECTS)
@@ -84,9 +147,9 @@ fclean: clean
 re: fclean all
 
 %.o: %.c
-    \$(CC) \$(CFLAGS) \$(INCLUDES) -c $< -o $@
+	\$(CC) \$(CFLAGS) \$(INCLUDES) -c $< -o $@
 
-.PHONY: \$(NAME) all clean fclean re
+.PHONY: \$(NAME) all directories clean fclean re
 " > submit/Makefile
 
 	if [ $# -eq 1 ]; then
