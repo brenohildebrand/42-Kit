da4df03b8c1e5f8ed5cfe1cd315d654cee3f566a
Breno Hildebrand <hello@brenohildebrand.com>
2024-03-13 17:00:32 -0300

    wow

--- build/default/dependencies/print.d
+++
@@ -1,9 +0,0 @@
-build/default/objects/print.o: source/functions/print.c \
- source/types/framework/framework.h source/functions/functions.h \
- source/types/i8/i8.h source/types/i32/i32.h source/types/u8/u8.h \
- source/types/u32/u32.h source/types/i64/i64.h source/types/u64/u64.h \
- source/types/any/any.h source/types/f64/f64.h source/types/chain/chain.h \
- source/types/list/list.h source/types/map/map.h \
- source/types/memtree/memtree.h source/types/pair/pair.h \
- source/types/string/string.h source/types/tuple/tuple.h \
- source/types/type/type.h source/functions/functions.h
